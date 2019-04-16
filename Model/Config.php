7148bec895f013ee56d3f25ca69c7e5e55521efe
Anthoula Wojczak <awojczak@adobe.com>
2019-04-16 15:13:27 -0500

    MC-13900 Email to a Friend updates
    - add form to configuration

--- Model/Config.php
+++ Model/Config.php
@@ -47,6 +47,7 @@ class Config
     const XML_PATH_ENABLED_FRONTEND_FORGOT = 'msp_securitysuite_recaptcha/frontend/enabled_forgot';
     const XML_PATH_ENABLED_FRONTEND_CONTACT = 'msp_securitysuite_recaptcha/frontend/enabled_contact';
     const XML_PATH_ENABLED_FRONTEND_CREATE = 'msp_securitysuite_recaptcha/frontend/enabled_create';
+    const XML_PATH_ENABLED_FRONTEND_SENDFRIEND = 'msp_securitysuite_recaptcha/frontend/enabled_sendfriend';
 
     /**
      * @var ScopeConfigInterface
@@ -165,6 +166,19 @@ class Config
         }
 
         return (bool) $this->scopeConfig->getValue(static::XML_PATH_ENABLED_FRONTEND_CREATE);
+    }
+
+    /**
+     * Return true if enabled on frontend send to friend
+     * @return bool
+     */
+    public function isEnabledFrontendSendFriend()
+    {
+        if (!$this->isEnabledFrontend()) {
+            return false;
+        }
+
+        return (bool) $this->scopeConfig->getValue(static::XML_PATH_ENABLED_FRONTEND_SENDFRIEND);
     }
 
     /**
